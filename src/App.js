2843a979422dae11c9d7b864c3b70342e0243152
Gustavo Mauricio <gmauricio207@gmail.com>
2021-08-06 20:14:07 -0300

    Requisiçao de API e criaçao de botao para favoritos

--- src/App.js
+++ src/App.js
@@ -1,30 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+let favoritos= JSON.parse(localStorage.getItem('favorito')) || [];
+
+export default class App extends Component {
+  constructor(){
+    super()
+
+
+    this.state ={
+      url:'',
+      status: 'loading'
+    }
+
+  }
+componentDidMount(){
+  this.getImage()
+  
 }
 
-export default App;
+ getImage = () => {
+  fetch('https://dog.ceo/api/breeds/image/random')
+  .then(response =>{ response.json().then((data) => {
+    
+    if (data.message.includes('terrier')) {
+      return this.setState({
+        url:'',
+        status:'loaded',
+      })
+    }
+
+    this.setState({
+     url:data.message,
+     status: 'loaded'
+   })
+  })
+ })  
+}
+
+
+// Salva nos Favoritos
+salveFavorito = () => {
+  const { url } = this.state;
+  favoritos.push(url)
+  localStorage.setItem('favorito', JSON.stringify(favoritos))
+}
+// Exclui dos Favoritos
+
+excluiFavoritos = () => {
+  const { url } = this.state;
+  
+  const index = localStorage.getItem('favorito').indexof(url);
+  
+  if (index != -1) {
+    favoritos.splice(index, 1)
+  }
+  
+}
+  render() {
+    const { url, status } = this.state
+    const carregando = <span>Carregando...</span>
+    
+    if (status === 'loading') {
+      return carregando
+    } 
+    return(
+      <div>
+        <img classNAme = 'dog'src={ url } alt='Dog'/>
+        <div class>
+          <button onClick={ this.getImage }>Novo DOG</button>
+          <button onClick={ this.salveFavorito }>Favorito</button>
+          <button onClick={ this.excluiFavoritos }>Excluir</button>
+        </div>
+      </div>
+    )
+  }
+}
+
